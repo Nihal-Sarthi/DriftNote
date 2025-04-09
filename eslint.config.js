f075e28b479c9dc7f5b1baee876d4c948ecdb3f7
“Nihal <nihalsarthi13@gmail.com>
2025-04-09 17:15:39 +0530

    changes to deploy to github pages

--- eslint.config.js
+++ eslint.config.js
@@ -31,9 +31,3 @@ export default [
     },
   },
 ]
-
-"homepage": "https://<your-username>.github.io/<repository-name>",
-"scripts": {
-  "predeploy": "npm run build",
-  "deploy": "gh-pages -d build"
-}
